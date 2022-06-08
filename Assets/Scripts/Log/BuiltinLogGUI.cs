c0c21ae40276c9cc2558bf07b073dfc8f6602da3
YangYuZhe <2684352466@qq.com>
2022-06-08 19:00:04 +0800

    补充Shader,onGUI尝试

---
+++ Assets/Scripts/Log/BuiltinLogGUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuiltinLogGUI : MonoBehaviour
+{
+    private List<string> _logStrBuffer = new List<string>();
+    private bool _showLogWindow = false;
+
+    #region 生命周期
+    void Start()
+    {
+            
+    }
+
+    private void OnGUI()
+    {
+        
+    }
+
+    private bool DrawToggle()
+    {
+        int width = 50;
+        int height = 50;
+        return GUI.Toggle(new Rect(Screen.width/2 - width/2,20,width,height),_showLogWindow,"ShowLog");
+    }
+    #endregion
+}
