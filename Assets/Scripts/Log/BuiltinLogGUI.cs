be8969587b6c94370b7417a0bb0c83513b4821fb
YangYuZhe <52652599+Ccimon@users.noreply.github.com>
2022-06-08 22:46:29 +0800

    做些尝试

--- Assets/Scripts/Log/BuiltinLogGUI.cs
+++ Assets/Scripts/Log/BuiltinLogGUI.cs
@@ -7,6 +7,7 @@ public class BuiltinLogGUI : MonoBehaviour
     private List<string> _logStrBuffer = new List<string>();
     private bool _showLogWindow = false;
 
+
     #region 生命周期
     void Start()
     {
@@ -15,14 +16,20 @@ public class BuiltinLogGUI : MonoBehaviour
 
     private void OnGUI()
     {
-        
+        _showLogWindow = DrawToggle();
+        if (_showLogWindow)
+        {
+            Debug.Log("????");
+            return;
+        }
+        Debug.Log("!!!!");
     }
 
     private bool DrawToggle()
     {
-        int width = 50;
-        int height = 50;
-        return GUI.Toggle(new Rect(Screen.width/2 - width/2,20,width,height),_showLogWindow,"ShowLog");
+        int width = 500;
+        int height = 300;
+        return GUI.Toggle(new Rect(Screen.width/2 - width/2,Screen.height/2,width,height),_showLogWindow,"ShowLog");
     }
     #endregion
 }
